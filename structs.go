4fab15918e619ea36ff38bf7fdac9e0d00bca922
Michael Peters <michaelpeterswa@icloud.com>
2021-09-18 00:47:25 -0700

    fix: typo

--- structs.go
+++ structs.go
@@ -3,6 +3,6 @@ package godapnet
 type Message struct {
 	Text              string   `json:"text"`
 	CallsignNames     []string `json:"callSignNames"`
-	TransmitterGroups []string `json:"transmitterGroups"`
+	TransmitterGroups []string `json:"transmitterGroupNames"`
 	Emergency         bool     `json:"emergency"`
 }
