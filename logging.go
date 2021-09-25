019ee3e06bd1a617da491664e4c133212ad1e679
Michael Peters <michaelpeterswa@icloud.com>
2021-09-24 21:02:10 -0700

    fix: remove version number

--- logging.go
+++ logging.go
@@ -7,8 +7,7 @@ import (
 var logger *zap.Logger
 
 func init() {
-	version := "v0.0.6"
 	logger, _ = zap.NewProduction()
 	defer logger.Sync()
-	logger.Info("godapnet is initializing...", zap.String("version", version))
+	logger.Info("godapnet is initializing...")
 }
